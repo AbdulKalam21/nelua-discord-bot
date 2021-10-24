c235971e2aeecfd5e5d12430369d5b4867fe3ca4
AKDev <akdevbusiness@gmail.com>
2021-10-24 20:23:25 +0530

    added compiling the nelua file

--- src/main/java/AKDev/main.java
+++ src/main/java/AKDev/main.java
@@ -1,9 +1,9 @@
 package AKDev;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.InputStreamReader;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -12,14 +12,11 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class main extends ListenerAdapter{
-    
-    FileWriter file;
 
     public static void main(String[] args) {
         
         try {
-            JDA jda = JDABuilder.createDefault("OTAxMzE0ODQxMDc4MjgwMjIy.YXOE2w.g5J1Lc_inYW7o9PgkPpDMFA59ps")
-                    .addEventListeners(new main()).build();
+            JDA jda = JDABuilder.createDefault("OTAxMzE0ODQxMDc4MjgwMjIy.YXOE2w.g5J1Lc_inYW7o9PgkPpDMFA59ps").addEventListeners(new main()).build();
             jda.awaitReady();
         } catch (LoginException ex){
             ex.printStackTrace();
@@ -33,23 +30,35 @@ public class main extends ListenerAdapter{
     public void onMessageReceived(MessageReceivedEvent event){
         
         Message msg = event.getMessage();
+        String[] output;    
+        FileWriter filewriter;
+        Process p;
+        String out;
+        String code;
         
-        if(msg.getContentRaw().startsWith("-compile")){
+        if(event.getMessage().getContentRaw().startsWith("-compile")){
             
-            String code = msg.getContentRaw().substring(15, msg.getContentRaw().lastIndexOf("```"));
+            code = msg.getContentRaw().substring(15, msg.getContentRaw().lastIndexOf("```"));
+            
             try {
-                file = new FileWriter("code.nelua");
-                file.write(code);
-            } catch (IOException ex) {
-                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            msg.getChannel().sendMessage("Compiling...").queue();
 
+                filewriter = new FileWriter("code.nelua");
+                filewriter.write(code);
+                filewriter.close();
+                msg.getChannel().sendMessage("Compiling...").queue();
+                p = Runtime.getRuntime().exec("cmd /c nelua code.nelua");
+            
+                BufferedReader reader = new BufferedReader(new InputStreamReader( p.getInputStream()));
+                
+                while((out = reader.readLine()) != null) {
+                    msg.getChannel().sendMessage(out).queue();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
             
         }
         
     }
     
 }
-
