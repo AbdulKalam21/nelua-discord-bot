e6bdf56d9f74b0df6ebac57b03645f621e7a9d2b
AKDev <akdevbusiness@gmail.com>
2021-11-02 20:56:17 +0530

    prgram will look for token in the environment variable

--- src/main/java/AKDev/main.java
+++ src/main/java/AKDev/main.java
@@ -3,6 +3,7 @@ package AKDev;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
@@ -21,26 +22,29 @@ public class main extends ListenerAdapter{
     public static File output;
     public static String code;
     public static FileWriter filewriter;
+    public static String BOT_TOKEN = System.getenv("BOT_TOKEN");
     
     public static void main(String[] args) {
         
         try {
-            JDA jda = JDABuilder.createDefault("BOT_TOKEN").addEventListeners(new main()).build();
+            JDA jda = JDABuilder.createDefault(BOT_TOKEN).addEventListeners(new main()).build();
             jda.awaitReady();
-        } catch (LoginException | InterruptedException ex){
+        } catch (LoginException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        
     }
     
     // BUG: The result is empty no matter what's the input
     // BUG: if there's a while loop runing, if another code starts running it won't run because the code.exe is already running
+    
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         msg = event.getMessage();
         if(msg.getContentRaw().startsWith("-compile")){
             code = msg.getContentRaw().substring(15, msg.getContentRaw().lastIndexOf("```"));
- 
+            
             try {
                 filewriter = new FileWriter("code.nelua");
                 filewriter.write(code);
@@ -49,9 +53,10 @@ public class main extends ListenerAdapter{
                 ex.printStackTrace();
             }
             
-            msg.reply("```Compiling...```").mentionRepliedUser(false).queue(response -> {    
+            msg.reply("```Compiling...```").mentionRepliedUser(false).queue(response -> {
+                
                 try {
-                    process = Runtime.getRuntime().exec("cmd /c nelua code.nelua > output.txt 2> error.txt");
+                    process = Runtime.getRuntime().exec("bash -c ./nelua code.nelua > output.txt 2> error.txt");
                     
                     if(process.waitFor(300, TimeUnit.SECONDS)){
                         error = new File("error.txt");
@@ -59,22 +64,24 @@ public class main extends ListenerAdapter{
                         if(error.length() != 0){
                             String result = Files.readString(Path.of("error.txt"));
                             response.editMessage("```" + result + "```").mentionRepliedUser(false).queue();
-                        }else if(output.length() != 0){
+                    }else if(output.length() != 0){
                             String result = Files.readString(Path.of("output.txt"));
                             response.editMessage("```" + result + "```").mentionRepliedUser(false).queue();
                         }
                     }else{
                         process.destroyForcibly();
                     }
+                
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
-                });
-             
+                
+            });
+            
+            
         }
-        
     }
     
 }
